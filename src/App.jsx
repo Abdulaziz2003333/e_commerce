8ca7f36d37c8cc7637c38233a497f865eb0acb69
Abdulaziz2003333 <lboy21044@gmail.com>
2024-01-29 22:22:39 +0500

    jonim

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import {MyContext} from "./context/Context.jsx";
 
 
 function App() {
-  const {count,setCount} = useContext(MyContext)
+  const {count,setCount,jonim} = useContext(MyContext)
 
   return (
     <>
     <h1>Salom </h1>
       <button onClick={() => setCount(count + 1)}>counter</button>
       <p>{count}</p>
+      <p>{jonim}</p>
     </>
   )
 }
--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -2,12 +2,11 @@ import { createContext, useState } from 'react';
 
 
 export const MyContext = createContext();
-
 const ContextProvider = ({ children }) => {
     const [count, setCount] = useState(0);
-
+const [jonim,setJonim]=useState('Jonimkam')
     return (
-        <MyContext.Provider value={{ count, setCount }}>
+        <MyContext.Provider value={{ count, setCount,jonim }}>
             {children}
         </MyContext.Provider>
     );
