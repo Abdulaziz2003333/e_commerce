5d4bbf1262b6e804538e2c33fa8aa8c21f1c4efd
sherzod-uralov <sherzoduralov432@gmail.com>
2024-01-29 22:19:52 +0500

    Context add

---
+++ src/context/Context.jsx
@@ -0,0 +1,16 @@
+import { createContext, useState } from 'react';
+
+
+export const MyContext = createContext();
+
+const ContextProvider = ({ children }) => {
+    const [count, setCount] = useState(0);
+
+    return (
+        <MyContext.Provider value={{ count, setCount }}>
+            {children}
+        </MyContext.Provider>
+    );
+};
+
+export default ContextProvider
