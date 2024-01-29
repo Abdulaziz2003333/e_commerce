884a59736cb553bbb5f6ed3949d7d98a2a611a34
sherzod-uralov <sherzoduralov432@gmail.com>
2024-01-29 22:25:31 +0500

    abs add

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import {MyContext} from "./context/Context.jsx";
 
 
 function App() {
-  const {count,setCount,jonim} = useContext(MyContext)
+  const {count,setCount,abs} = useContext(MyContext)
 
   return (
     <>
     <h1>Salom </h1>
       <button onClick={() => setCount(count + 1)}>counter</button>
       <p>{count}</p>
-      <p>{jonim}</p>
+      <p>{abs}</p>
+      <p>men seni dadangmani xaromi</p>
     </>
   )
 }
--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -4,9 +4,9 @@ import { createContext, useState } from 'react';
 export const MyContext = createContext();
 const ContextProvider = ({ children }) => {
     const [count, setCount] = useState(0);
-const [jonim,setJonim]=useState('Jonimkam')
+const [abs,setJonim]=useState('Jonimkam')
     return (
-        <MyContext.Provider value={{ count, setCount,jonim }}>
+        <MyContext.Provider value={{ count, setCount,abs }}>
             {children}
         </MyContext.Provider>
     );
