dac06beb7b5ddc447dca2fd4df5118f035cba935
Abdulaziz2003333 <lboy21044@gmail.com>
2024-01-29 21:55:18 +0500

    Test for yanvar
    test P
    
    (cherry picked from commit de94a1255422148f23fa19a819bfd5f4c8a5fe49)

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import {useContext} from "react";
-import {MyContext} from "./context/Context.jsx";
+
 
 
 function App() {
-  const {count,setCount} = useContext(MyContext)
+
 
   return (
     <>
     <h1>Salom </h1>
-      <button onClick={() => setCount(count + 1)}>counter</button>
-     
     </>
   )
 }
