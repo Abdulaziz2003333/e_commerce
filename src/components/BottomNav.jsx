43b706c15c9d1eed930944de4e5bb935d6ff6f72
Abdulaziz2003333 <lboy21044@gmail.com>
2024-02-01 18:31:08 +0500

    jonim1

---
+++ src/components/BottomNav.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Link from 'react-router-dom'
+const BottomNav = () => {
+    return (
+        <div className="flex">
+            <div><span>Exclusive</span></div>
+            <div>
+                <ul className="flex">
+                    <li><Link to={'/'}>Home</Link></li>
+                    <li><Link to={'/contact'}>Contact</Link></li>
+                    <li><Link to={'/about'}>About</Link></li>
+                    <li><Link to={'/login'}>Sign Up</Link></li>
+                </ul>
+            </div>
+            <div>
+            <input type="text" placeholder="What are you looking for"/>
+            </div>
+        </div>
+    );
+};
+
+export default BottomNav;
