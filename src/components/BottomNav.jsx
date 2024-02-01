b87cd74447b39aff1bceb1f0707517de040fd987
Abdulaziz2003333 <lboy21044@gmail.com>
2024-02-01 20:41:44 +0500

    Jonim3

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -1,19 +1,22 @@
 
 import {Link} from 'react-router-dom'
+import {useTranslation} from "react-i18next";
+
 const BottomNav = () => {
+    const {t}=useTranslation()
     return (
-        <div className="flex">
+        <div className="flex container items-center justify-around">
             <div><span>Exclusive</span></div>
             <div>
-                <ul className="flex">
-                    <li><Link to={'/'}>Home</Link></li>
-                    <li><Link to={'/'}>Contact</Link></li>
-                    <li><Link to={'/'}>About</Link></li>
-                    <li><Link to={'/'}>Sign Up</Link></li>
+                <ul className="flex gap-14">
+                    <li><Link to={'/'}>{t('bottomNav.home')}</Link></li>
+                    <li><Link to={'/'}>{t('bottomNav.contact')}</Link></li>
+                    <li><Link to={'/'}>{t('bottomNav.about')}</Link></li>
+                    <li><Link to={'/'}>{t('bottomNav.signUp')}</Link></li>
                 </ul>
             </div>
             <div>
-            <input type="text" placeholder="What are you looking for"/>
+                <input type="text" placeholder="What are you looking for" className="bg-white"/>
             </div>
         </div>
     );
